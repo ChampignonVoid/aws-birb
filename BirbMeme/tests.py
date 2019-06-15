51578666cabe929ec74fd9073e304c44175a4c6f
Laxman Thayalan <laxman.thayalan@epita.fr>
2019-06-15 18:14:15 +0200

    Tests updated (check if it does compile)

--- BirbMeme/tests.py
+++ BirbMeme/tests.py
@@ -13,12 +13,6 @@ from django.test import TestCase
 class SimpleTest(TestCase):
     """Tests for the application views."""
 
-    # Django requires an explicit setup() when running tests in PTVS
-    @classmethod
-    def setUpClass(cls):
-        super(SimpleTest, cls)
-        django.setup()
-
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
