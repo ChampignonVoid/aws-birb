48d37c5960c4957819dc5347acae5246c8101d8a
Laxman Thayalan <laxman.thayalan@epita.fr>
2019-06-15 22:01:28 +0200

    Add database access for heroku

--- DjangoWebProject/settings.py
+++ DjangoWebProject/settings.py
@@ -90,6 +90,17 @@ DATABASES = {
     }
 }
 
+if not DEBUG:
+    DATABASES = {
+        'default' : {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'd4s205ctoo1ac2',
+            'USER': 'yfdcnacnhclxja',
+            'PASSWORD': '7310e41e804f471ab424e30d95603e5e828f6afb0706116968bfd4230f4a4a1f',
+            'HOST': 'ec2-54-217-225-16.eu-west-1.compute.amazonaws.com',
+            'PORT': '5432'
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
